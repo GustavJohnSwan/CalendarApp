db7a3ffaee43f35d9219ecb398f96c8ff181bc5e
Agressive_Swan <gustavs.janis.gulbis@gmail.com>
2026-07-03 19:11:11 +0300

    Calendar App v10 - MVP_Benchmark_V1
    SQLite Room - Fully implemented
    CouchBase Lite - Fully implemented
    ObjectBox - Fully implemented
    
    Code cleanup - Fully cleaned
    
    Basic CRUD benchmark functionality - Fully implemented (TESTING - REMOVE THIS)
    
    CBL DAO benchmark optimisation - Fully improved (doc-by-doc --> bulk edits)
    _____________________________________________
    
    - Create queries of increased complexity

---
+++ app/src/main/java/com/bignerdranch/android/calendarapp3/database_2/objectbox/models.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.calendarapp3.database_2.objectbox
+
+import androidx.room.Entity
+import io.objectbox.annotation.Id
+
+@Entity
+data class User(
+    @Id
+    var id: Long = 0,
+    var name: String? = null
+)
+
+@Entity
+data class Household(
+    @Id
+    var id: Long = 0,
+    var address: String? = null,
+    var color: String? = null,
+    var stories: Int = 1
+)

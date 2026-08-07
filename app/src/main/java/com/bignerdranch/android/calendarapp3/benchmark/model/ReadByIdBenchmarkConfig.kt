9d1435dec42e0876798575694e5939ed670fc0ae
Agressive_Swan <gustavs.janis.gulbis@gmail.com>
2026-08-07 17:54:23 +0300

    Calendar App v10 - MVP_Benchmark_V1
    SQLite Room - Fully implemented
    CouchBase Lite - Fully implemented
    ObjectBox - Fully implemented
    
    Code cleanup - Fully cleaned
    
    Basic CRUD benchmark functionality - Fully implemented (TESTING - REMOVE THIS)
    
    CBL DAO benchmark optimisation - Fully improved (doc-by-doc --> bulk edits)
    _____________________________________________
    
    For benchmarking :
    - Recreate fundamental objectbox database
    - Recreate fundamental objectbox DAO
    
    - Recreate fundamental CBL database
    - Recreate fundamental CBL DAO
    
    - Recreate fundamental Room database
    - Recreate fundamental Room DAO
    
    - Create queries for benchmarking

--- app/src/main/java/com/bignerdranch/android/calendarapp3/benchmark/model/ReadByIdBenchmarkConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.bignerdranch.android.calendarapp3.benchmark.model
-
-data class ReadByIdBenchmarkConfig(
-    val entryCount: Int,
-    val lookupsPerRun: Int,
-    val warmupRuns: Int,
-    val measuredRuns: Int
-)
--- app/src/main/java/com/bignerdranch/android/calendarapp3/database/DB_DATABASE.kt
+++ app/src/main/java/com/bignerdranch/android/calendarapp3/database/DB_DATABASE.kt
@@ -4,27 +4,21 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.bignerdranch.android.calendarapp3.benchmark.room.BenchmarkRoomDao
 import com.bignerdranch.android.calendarapp3.database.DAO.AttachmentDao
 import com.bignerdranch.android.calendarapp3.database.DAO.EntryDao
 import com.bignerdranch.android.calendarapp3.database.DAO.ExtraDataDao
-import com.bignerdranch.android.calendarapp3.benchmark.room.BenchmarkRoomEntity
-import com.bignerdranch.android.calendarapp3.benchmark.room.BenchmarkRoomIndexedDao
-import com.bignerdranch.android.calendarapp3.benchmark.room.BenchmarkRoomIndexedEntity
+
 
 // this class sets up the database - defines its configuration and acts as the main access point to the data
 // version needs to be incremented every time the schema is updated/changed
-@Database(entities = [EntryTable::class, ExtraDataTable::class, EntryAttachment::class, BenchmarkRoomEntity::class, BenchmarkRoomIndexedEntity::class], version = 16)
+@Database(entities = [EntryTable::class, ExtraDataTable::class, EntryAttachment::class], version = 17)
 abstract class AppDatabase : RoomDatabase() { // AppDatabase extends RoomDatabase
     // returns DAO interfaces
     abstract fun entryDao(): EntryDao
     abstract fun extraDataDao(): ExtraDataDao
     abstract fun attachmentDao(): AttachmentDao
 
-    // for testing basic CRUD for Room
-    abstract fun benchmarkRoomDao(): BenchmarkRoomDao
 
-    abstract fun benchmarkRoomIndexedDao(): BenchmarkRoomIndexedDao
 
 
     // this creates a shared, static variable called INSTANCE. It holds the only copy of the database. It is a SINGLETON that allows only one INSTANCE to exist in the whole app
